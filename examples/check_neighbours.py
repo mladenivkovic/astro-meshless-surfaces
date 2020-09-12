2a0b40f8d8bb8f4a76076b081602fd80c009ad10
mladenivkovic <mladen.ivkovic@hotmail.com>
2020-09-12 22:15:15 +0200

    formatted example scripts

--- examples/check_neighbours.py
+++ examples/check_neighbours.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 
 
-#===============================================================
+# ===============================================================
 # Get neighbour lists in naive way by looping over all
 # particles and by sorting them out
-#===============================================================
+# ===============================================================
 
 
 import numpy as np
@@ -18,28 +18,20 @@ except ImportError:
     quit(2)
 
 
-
-
-
-#---------------------------
+# ---------------------------
 # initialize variables
-#---------------------------
+# ---------------------------
 
 
 # temp during rewriting
 #  srcfile = './snapshot_perturbed.hdf5'    # swift output file
 #  srcfile = './snapshot_uniform.hdf5'    # swift output file
-srcfile = './snapshot_sodshock.hdf5'    # swift output file
-ptype = 'PartType0'                 # for which particle type to look for
+srcfile = "./snapshot_sodshock.hdf5"  # swift output file
+ptype = "PartType0"  # for which particle type to look for
 
 
-
-
-
-#========================
 def main():
-#========================
-    
+
     # get data
     x, y, h, rho, m, ids, npart = ms.read_file(srcfile, ptype, sort=True)
     #  x, y, h, rho, m, ids, npart = ms.read_file(srcfile, ptype)
@@ -48,14 +40,15 @@ def main():
     # get neighbours
     print("Computing Naive")
     start_n = time.time()
-    ndata_naive = ms.get_neighbour_data_for_all_naive(x, y, H, fact=1.0, L=1.0, periodic=True)
+    ndata_naive = ms.get_neighbour_data_for_all_naive(
+        x, y, H, fact=1.0, L=1.0, periodic=True
+    )
     stop_n = time.time()
 
     print("Computing better")
     start_s = time.time()
     ndata_smart = ms.get_neighbour_data_for_all(x, y, H, fact=1.0, L=1.0, periodic=True)
     stop_s = time.time()
-
 
     # compare results
     mx_n = ndata_naive.maxneigh
@@ -75,7 +68,13 @@ def main():
         nn = nneigh_n[p]
         ns = nneigh_s[p]
         if nn != ns:
-            print("Got different number of neighbours for particle ID", ids[p], ":", nn, ns)
+            print(
+                "Got different number of neighbours for particle ID",
+                ids[p],
+                ":",
+                nn,
+                ns,
+            )
             print(ids[neigh_n[p]])
             print(ids[neigh_s[p]])
 
@@ -84,14 +83,14 @@ def main():
                 nl = nn
                 smaller = neigh_s
                 nsm = ns
-                larger_is = 'naive'
+                larger_is = "naive"
             else:
                 larger = neigh_s
                 nl = ns
                 smaller = neigh_n
                 nsm = nn
-                larger_is = 'smart'
-                
+                larger_is = "smart"
+
             i = 0
             while i < nl:
                 if larger[p][i] != smaller[p][i]:
@@ -104,20 +103,31 @@ def main():
             Hl = H[larger[p][problem]]
             idl = ids[larger[p][problem]]
             dxl, dyl = ms.get_dx(xl, x[p], yl, y[p])
-            rl = np.sqrt(dxl**2 + dyl**2)
+            rl = np.sqrt(dxl ** 2 + dyl ** 2)
 
             xS = x[smaller[p][problem]]
             yS = y[smaller[p][problem]]
             HS = H[smaller[p][problem]]
             idS = ids[smaller[p][problem]]
             dxS, dyS = ms.get_dx(xS, x[p], yS, y[p])
-            rS = np.sqrt(dxS**2 + dyS**2)
-    
+            rS = np.sqrt(dxS ** 2 + dyS ** 2)
 
             print("Larger is:", larger_is, " positions:")
-            print("ID part:  {0:6d}  x: {1:14.7f}  y: {2:14.7f}  H: {3:14.7f}".format(ids[p], x[p], y[p], H[p]))
-            print("ID large: {0:6d}  x: {1:14.7f}  y: {2:14.7f}  r: {3:14.7f}  H: {4:14.7f} r/H: {5:14.7f}".format(idl, xl, yl, rl, Hl, rl/Hl))
-            print("ID small: {0:6d}  x: {1:14.7f}  y: {2:14.7f}  r: {3:14.7f}  H: {4:14.7f} r/H: {5:14.7f}".format(idS, xS, yS, rS, HS, rS/HS))
+            print(
+                "ID part:  {0:6d}  x: {1:14.7f}  y: {2:14.7f}  H: {3:14.7f}".format(
+                    ids[p], x[p], y[p], H[p]
+                )
+            )
+            print(
+                "ID large: {0:6d}  x: {1:14.7f}  y: {2:14.7f}  r: {3:14.7f}  H: {4:14.7f} r/H: {5:14.7f}".format(
+                    idl, xl, yl, rl, Hl, rl / Hl
+                )
+            )
+            print(
+                "ID small: {0:6d}  x: {1:14.7f}  y: {2:14.7f}  r: {3:14.7f}  H: {4:14.7f} r/H: {5:14.7f}".format(
+                    idS, xS, yS, rS, HS, rS / HS
+                )
+            )
             quit()
 
         for n in range(nneigh_n[p]):
@@ -132,14 +142,12 @@ def main():
     if not found_difference:
         print("Found no difference.")
 
-
     print()
-    print('{0:18} {1:18} {2:18}'.format("time naive", "time better", "naive/better"))
+    print("{0:18} {1:18} {2:18}".format("time naive", "time better", "naive/better"))
     tn = stop_n - start_n
     ts = stop_s - start_s
-    print('{0:18.4f} {1:18.4f} {2:18.4f}'.format(tn, ts, tn/ts))
+    print("{0:18.4f} {1:18.4f} {2:18.4f}".format(tn, ts, tn / ts))
 
-    
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
--- examples/example_hopkins.py
+++ examples/example_hopkins.py
@@ -1,15 +1,15 @@
 #!/usr/bin/env python3
 
 
-#===============================================================
+# ===============================================================
 # Visualize the effective area from a uniform distribution
 # where the smoothing lengths have been computed properly.
 #
-# For a chosen particle, for each neighbour within 2h the 
+# For a chosen particle, for each neighbour within 2h the
 # effective surface is plotted as vectors in the plane
 #
 # Written by Mladen Ivkovic 2019
-#===============================================================
+# ===============================================================
 
 
 import numpy as np
@@ -22,54 +22,48 @@ except ImportError:
     quit(2)
 
 
-
-#---------------------------
+# ---------------------------
 # initialize variables
-#---------------------------
+# ---------------------------
 
 
 # temp during rewriting
-srcfile = './snapshot_uniform.hdf5'    # swift output file
-ptype = 'PartType0'                 # for which particle type to look for
-pcoord = [0.5, 0.5]                 # coordinates of particle to work for
-pind = None                         # index of particle you chose with pcoord
+srcfile = "./snapshot_uniform.hdf5"  # swift output file
+ptype = "PartType0"  # for which particle type to look for
+pcoord = [0.5, 0.5]  # coordinates of particle to work for
+pind = None  # index of particle you chose with pcoord
 npart = 0
 
-nbors = []                          # indices of all relevant neighbour particles
+nbors = []  # indices of all relevant neighbour particles
 
 
-
-
-fullcolorlist=['red', 
-        'green', 
-        'blue', 
-        'gold', 
-        'magenta', 
-        'cyan',
-        'lime',
-        'saddlebrown',
-        'darkolivegreen',
-        'cornflowerblue',
-        'orange',
-        'dimgrey',
-        'navajowhite',
-        'darkslategray',
-        'mediumpurple',
-        'lightpink',
-        'mediumseagreen',
-        'maroon',
-        'midnightblue',
-        'silver']
+fullcolorlist = [
+    "red",
+    "green",
+    "blue",
+    "gold",
+    "magenta",
+    "cyan",
+    "lime",
+    "saddlebrown",
+    "darkolivegreen",
+    "cornflowerblue",
+    "orange",
+    "dimgrey",
+    "navajowhite",
+    "darkslategray",
+    "mediumpurple",
+    "lightpink",
+    "mediumseagreen",
+    "maroon",
+    "midnightblue",
+    "silver",
+]
 
 ncolrs = len(fullcolorlist)
 
 
-
-
-#========================
 def main():
-#========================
-    
 
     # read data from snapshot
     x, y, h, rho, m, ids, npart = ms.read_file(srcfile, ptype)
@@ -87,48 +81,50 @@ def main():
     A_ij = ms.Aij_Hopkins(pind, x, y, H, m, rho)
     x_ij = ms.x_ij(pind, x, y, H, nbors=nbors)
 
-    
-
     print("Plotting")
 
-    fig = plt.figure(figsize=(12,12))
+    fig = plt.figure(figsize=(12, 12))
     ax1 = fig.add_subplot(111)
 
     pointsize = 200
     # plot central particle
-    ax1.scatter(x[pind], y[pind], c='k', s=pointsize*2)
+    ax1.scatter(x[pind], y[pind], c="k", s=pointsize * 2)
 
     # plot other particles and arrows
-    for i,n in enumerate(nbors):
+    for i, n in enumerate(nbors):
         cc = i
         while cc > ncolrs:
             cc -= ncolrs
         col = fullcolorlist[cc]
 
-        ax1.scatter(x[n], y[n], c=col, s=pointsize, zorder=0, lw=1, edgecolor='k')
+        ax1.scatter(x[n], y[n], c=col, s=pointsize, zorder=0, lw=1, edgecolor="k")
         arrind = 2
-        arrwidth = arrind*2
-        ax1.arrow(x_ij[i][0], x_ij[i][1], A_ij[i][0], A_ij[i][1], 
-            color=col, lw=arrwidth, zorder=100-arrind)
+        arrwidth = arrind * 2
+        ax1.arrow(
+            x_ij[i][0],
+            x_ij[i][1],
+            A_ij[i][0],
+            A_ij[i][1],
+            color=col,
+            lw=arrwidth,
+            zorder=100 - arrind,
+        )
 
+    ax1.set_facecolor("lavender")
+    ax1.set_xlim((0.25, 0.75))
+    ax1.set_ylim((0.25, 0.75))
 
-    ax1.set_facecolor('lavender')
-    ax1.set_xlim((0.25,0.75))
-    ax1.set_ylim((0.25,0.75))
-
-    ax1.set_title(r'Hopkins $\mathbf{A}_{ij}$ at $\mathbf{x}_{ij} = \mathbf{x}_i + \frac{h_i}{h_i+h_j}(\mathbf{x}_j - \mathbf{x}_i)$', fontsize=18, pad=18)
-    ax1.set_xlabel('x')
-    ax1.set_ylabel('y')
-
+    ax1.set_title(
+        r"Hopkins $\mathbf{A}_{ij}$ at $\mathbf{x}_{ij} = \mathbf{x}_i + \frac{h_i}{h_i+h_j}(\mathbf{x}_j - \mathbf{x}_i)$",
+        fontsize=18,
+        pad=18,
+    )
+    ax1.set_xlabel("x")
+    ax1.set_ylabel("y")
 
     plt.show()
     #  plt.savefig('effective_area_all_neighbours.png', dpi=200)
 
 
-
-
-
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
-
